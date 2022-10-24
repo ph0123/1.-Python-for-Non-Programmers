d5a914c9bcf51a0ee8046a1a21eb0c0af79b943e
ph0123 <cnphuongdavid@gmail.com>
2022-10-24 17:50:13 +0200

    chapter 1 and 2 update full

--- 2. project1_ Fortune Cookie.py
+++ 2. project1_ Fortune Cookie.py
@@ -1,5 +1,19 @@
 #1 picking random numbers
+import random
+x = random.randint(1,10)
+print(x)
 
+print(random.random()) #will return float value
 
+x = random.randint(1,3)
+if x == 1:
+    print("Yes")
+elif x == 2:
+    print("No")
+else:
+    print("Maybe")
 
 #2.  choosing what fortune to show
+
+x = random.randint(1,100)
+print(f'Your lucky number for today is {x}')
