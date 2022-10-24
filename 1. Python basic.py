498cf909f0c6e0c3fb3031d2b23e5ea693b39d8c
ph0123 <cnphuongdavid@gmail.com>
2022-10-24 17:37:10 +0200

    first commit

---
+++ 1. Python basic.py
@@ -0,0 +1,36 @@
+#1 Variables
+i =2
+print(i)
+
+#2. Numbers: Ints and floats.
+a = 1
+b = 1.5
+c = -10
+print(a,b,c)
+
+#3. String
+
+shirt = "blue"
+print(shirt)
+
+store = "Bick's Pizza Shop, the \"best\" there is"
+print(shirt)
+print(store)
+
+
+a = 1
+b = 1.5
+c = -10
+print(f"He is {a} year old")
+
+
+#4. booleans and if statements.
+
+#5 comparison and else
+#if elif else...
+
+light_on = True
+if light_on:
+    print('On')
+else:
+    print('off')
